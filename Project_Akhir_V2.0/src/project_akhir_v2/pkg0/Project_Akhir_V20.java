65e65b753edb11e5d832a894d61ea57499a5eaa5
boim212 <bimaboim7@gmail.com>
2022-05-24 14:51:32 +0700

    first Commit

---
+++ Project_Akhir_V2.0/src/project_akhir_v2/pkg0/Project_Akhir_V20.java
@@ -0,0 +1,33 @@
+package project_akhir_v2.pkg0;
+public class Project_Akhir_V20 {
+    public static void main(String[] args) {
+        JFrame_MenuUtama u = new JFrame_MenuUtama();
+        u.setVisible(true);
+        
+//        GUI_Tree phn = new GUI_Tree();
+//        String[] kata = {"AKAR", "BAJU", "BANDAR", "CERITA", "CENDAWAN", "ABAD",
+//            "ALUR", "BERDALIH", "BERSUA"};
+//        String[] arti = {"Bagian tumbuhan yang tertanam di dalam tanah", "Pakaian"
+//            + " penutup badan bagian atas", "Orang yang bermodal dalam "
+//            + "perdagangan", "Omong kosong", "Jamur yang besar", "Masa "
+//            + "Seratur Tahun", "Rangakaian peristiwa yang direka dan di "
+//            + "jalin dengan seksama dan menggerakkan jalan cerita melalui "
+//            + "kerumitan ke arah klimaks dan penyelesaian", "Mengemukakan "
+//            + "alasan (yang dicari-cari) untuk menghindari tugas atau menutupi "
+//            + "perbuatan yang salah atau tercela", "Data saling mendekati "
+//            + "(berdekat-dekatan)"};
+//        for (int i = 0; i < kata.length; i++) {
+//            phn.insert(kata[i], arti[i]);
+//            System.out.println("Data Berhasil Di Tambahkan");
+//        }
+//        
+//        System.out.println("");
+//        System.out.print("In A-Z:");
+//        phn.inOrderTraversal();
+//        System.out.println("");
+//        System.out.print("In Z-A :");
+//        phn.inOrderTraversal_Revese();
+//        System.out.println("");
+    }
+    
+}
